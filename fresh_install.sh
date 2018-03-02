1acd46a59858e9acd79a368a86403f4d0a18d31f
Innursery <in.nursery@gmail.com>
2018-03-02 07:48:05 -0500

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -5,9 +5,8 @@ echo "Installing PHP5-CLI CURL"
 apt-get -y install php5-cli curl
 echo "Done"
 cd /tmp
-wget http://fos-streaming.com/install.php.1
-mv install.php.1 install.php
-php install.php
-wget http://fos-streaming.com/db_install.sh
+wget https://raw.githubusercontent.com/Innursery/FOS-Streaming-v1-1/master/install_panel.php
+php install_panel.php
+wget https://raw.githubusercontent.com/Innursery/FOS-Streaming-v1-1/master/db_install.sh
 chmod 755 db_install.sh
 ./db_install.sh
